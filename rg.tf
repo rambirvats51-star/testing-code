8629dd14881ad58ae21ef402891ee2187788db3c
Rambir Vats <rambirvats51@gmail.com>
2026-06-22 23:52:18 +0530

    rg

---
+++ rg.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "ram-rg-1"
+  location = "Central India"
+
+  
+  }
